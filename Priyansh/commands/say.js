3edaa624bd74dd618ae70aa4df17167a109bf910
raihan9099 <140356472+raihan9099@users.noreply.github.com>
2025-08-08 14:07:55 +0600

    Update say.js

--- Priyansh/commands/say.js
+++ Priyansh/commands/say.js
@@ -18,7 +18,7 @@ module.exports.run = async function({ api, event, args }) {
 		const { createReadStream, unlinkSync } = global.nodemodule["fs-extra"];
 		const { resolve } = global.nodemodule["path"];
 		var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
-		var languageToSay = (["ru","en","pr","ja", "tl"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
+		var languageToSay = (["bn"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
 		var msg = (languageToSay != global.config.language) ? content.slice(3, content.length) : content;
 		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
 		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=${languageToSay}&client=tw-ob`, path);
